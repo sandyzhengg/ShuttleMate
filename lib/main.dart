795ce14ed5fd882fea7a6159c22db7c37f0d12c7
Peter Bortis <you@example.com>
2024-12-29 22:05:34 -0600

    Layout formatting

--- lib/main.dart
+++ lib/main.dart
@@ -122,152 +122,163 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
-    final double screenHeight = MediaQuery.of(context).size.height;
+  final double screenWidth = MediaQuery.of(context).size.width;
+  final double screenHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: GestureDetector(
-          onTap: _launchURL,
-          child: Container(
-            width: screenWidth * 0.9,
-            padding: const EdgeInsets.all(3.0), // Add padding if needed
-            child: Image.asset(
-              'assets/ShuttleMateLogo.png',
-              fit: BoxFit.contain, // Ensure the image fits within the container
+  return Scaffold(
+    appBar: AppBar(
+      title: GestureDetector(
+        onTap: _launchURL,
+        child: Container(
+          width: screenWidth * 0.9,
+          height: 400,
+          padding: const EdgeInsets.all(5), // Add padding if needed
+          child: Image.asset(
+            'assets/ShuttleMateLogo.png',
+            fit: BoxFit.contain, // Ensure the image fits within the container
+          ),
+        ),
+      ),
+      backgroundColor: Color(0xFF800000),
+    ),
+    body: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xFF800000), Color(0xFF4C0202)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomLeft,
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          // First section with Dropdown and Text
+          SizedBox(
+            height: screenHeight * 0.125,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "Going Somewhere?",
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: screenWidth * 0.05,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: screenWidth * 0.025),
+                    DropdownButtonHideUnderline(
+                      child: Container(
+                        width: screenWidth * 0.5,
+                        height: screenWidth * 0.1,
+                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(50),
+                          border: Border.all(color: Color(0xFF303030), width: 4),
+                        ),
+                        child: DropdownButton<String>(
+                          isExpanded: true,
+                          value: dropdownValue,
+                          icon: const Icon(Icons.arrow_drop_down),
+                          iconSize: 24,
+                          style: const TextStyle(color: Colors.black),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownValue = newValue!;
+                            });
+                          },
+                          items: dropdownOptions.map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Row(
+                                children: [
+                                  Text(
+                                    value,
+                                    style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 7,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Daytime",
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: screenWidth * 0.05,
+                        color: Colors.white,
+                      ),
+                    ),
+                    // Add more widgets to Column 2 as needed
+                  ],
+                ),
+              ],
             ),
           ),
-        ),
-        backgroundColor: Color(0xFF800000),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFF800000), Color(0xFF4C0202)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomLeft,
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-              Column(
-                    children: [
-                      Text(
-                        "Going Somewhere?",
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
-                      ),
-                      DropdownButtonHideUnderline(
-                        child: Container(
-                          width: screenWidth * 0.5,
-                          height: screenWidth * 0.1,
-                          padding: EdgeInsets.symmetric(horizontal: 2, vertical: 0),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border.all(color: Color(0xFF303030), width: 4),
-                          ),
-                          child: DropdownButton<String>(
-                            value: dropdownValue,
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                dropdownValue = newValue!;
-                              });
-                            },
-                            items: dropdownOptions.map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      value,
-                                      style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                        fontSize: 8,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "Daytime",
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      ),
-                      // Add more widgets to Column 2 as needed
-                    ],
-                  ),
-              ],
+          // WebView Container
+          Container(
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.black,
+                  width: 5, // Set the desired border width here
+                ),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.black,
-                      width: 5, // Set the desired border width here
+            ),
+            child: SizedBox(
+              width: screenWidth * 1,
+              height: screenHeight * 0.45,
+              child: WebViewWidget(controller: controller),
+            ),
+          ),
+          // Shuttle Stops Text
+          SizedBox(
+            height: screenHeight * 0.05,
+            child: Text(
+              "Shuttle Stops",
+              style: TextStyle(
+                fontFamily: 'DelaGothicOne',
+                fontSize: screenWidth * 0.05,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          // GridView with buttons
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5, // 5 columns
+              ),
+              itemCount: buttonData.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.all(screenWidth * 0.02), // Add margin between buttons
+                  child: Tooltip(
+                    message: '${buttonData[index]['label']}', // Display button index and label
+                    child: BorderToggleButton(
+                      label: buttonData[index]['label'],
+                      color: buttonData[index]['color'],
                     ),
                   ),
-                ),
-                child: SizedBox(
-                  width: screenWidth * 1, // Set the desired width here
-                  height: screenHeight * 0.5, // Set the desired height here
-                  child: WebViewWidget(
-                    controller: controller,
-                  ),
-                ),
-              ),
-              Text(
-                "Shuttle Stops",
-                  style: TextStyle(
-                  fontFamily: 'DelaGothicOne',
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
-              ),
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 5, // 5 columns
-                  ),
-                  itemCount: buttonData.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                    padding: const EdgeInsets.all(8.0), // Add margin between buttons
-                    child: Tooltip(
-                      message: '${buttonData[index]['label']}', // Display button index and label
-                      child: BorderToggleButton(
-                        label: buttonData[index]['label'],
-                        color: buttonData[index]['color'],
-                      ),
-                    ),
-                  );
-                  },
-                ),
-              ),
-            ],
+                );
+              },
+            ),
           ),
-        ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
 }
