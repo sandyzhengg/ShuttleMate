f28932d0377c9c13df7c2e6e9a574fb64fcf599d
sandyzhengg <128326330+sandyzhengg23@users.noreply.github.com>
2024-12-29 18:45:46 -0600

    started on daytime/nighttime toggle functionality

--- lib/main.dart
+++ lib/main.dart
@@ -8,27 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
         useMaterial3: true,
       ),
@@ -40,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -56,6 +31,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isDaytime = true;
+
   Future<void> _launchURL() async {
     final Uri url = Uri.parse('https://chicago.passiogo.com/');
     if (await canLaunchUrl(url)) {
@@ -63,11 +40,18 @@ class _MyHomePageState extends State<MyHomePage> {
     } else {
       throw 'Could not launch $url';
     }
-}
+  }
 
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
+
+    String imagePath;
+    if (isDaytime) {
+      imagePath = 'assets/daytime.png';
+    } else {
+      imagePath = 'assets/nighttime.png';
+    }
 
     return Scaffold(
       body: Container(
@@ -89,11 +73,33 @@ class _MyHomePageState extends State<MyHomePage> {
                   width: screenWidth * 0.9,
                 ),
               ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Daytime', style: TextStyle(color: Colors.white)),
+                  Switch(
+                    value: isDaytime,
+                    onChanged: (value) {
+                      setState(() {
+                        isDaytime = value;
+                      });
+                    },
+                    activeColor: Colors.yellow,
+                    inactiveThumbColor: Colors.blueGrey,
+                  ),
+                  const Text('Nighttime', style: TextStyle(color: Colors.white)),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Image.asset(
+                imagePath,
+                width: screenWidth * 0.9,
+              ),
             ],
           ),
         ),
       ),
-
     );
   }
 }
