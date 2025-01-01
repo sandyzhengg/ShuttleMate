76f189c92c61da603a002ab03eb839745532eb65
Peter Bortis <you@example.com>
2025-01-01 00:03:09 -0600

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shuttlemate_app/main.dart';
+import 'package:shuttlemate/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
